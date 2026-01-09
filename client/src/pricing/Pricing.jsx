cb19dc863fb7046b5c3eb780104bd3546518ff25
bobbyku0104 <143bobby123@gmail.com>
2026-01-09 15:08:27 +0530

    remove some extra codes

--- client/src/pricing/Pricing.jsx
+++ client/src/pricing/Pricing.jsx
@@ -1,64 +1,10 @@
-// import PricingCard from "./PricingCard";
-
-// export default function Pricing() {
-//   return (
-//     <section className="relative bg-[#0b1220] text-white py-24 px-6 overflow-hidden">
-//       {/* Header */}
-//       <div className="text-center max-w-3xl mx-auto mb-16">
-//         <span className="inline-block px-5 py-2 text-sm rounded-full border border-indigo-500/40 text-indigo-400 mb-6">
-//           PRICING
-//         </span>
-
-//         <h2 className="text-4xl md:text-5xl font-bold mb-4">
-//           Simple, Transparent Pricing
-//         </h2>
-
-//         <p className="text-gray-400 text-lg">
-//           Choose the plan that fits your business needs
-//         </p>
-//       </div>
-
-//       {/* Cards */}
-//       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
-//         <PricingCard
-//           title="Starter"
-//           price="$99"
-//           subtitle="/month"
-//           features={["Up to $5k ad spend", "Google & Meta AI optimization"]}
-//         />
-
-//         <PricingCard
-//           title="Growth"
-//           price="$299"
-//           subtitle="/month"
-//           popular
-//           features={["Up to $25k ad spend", "All platform optimization"]}
-//         />
-
-//         <PricingCard
-//           title="Enterprise"
-//           price="Custom"
-//           features={["Unlimited ad spend", "Custom AI model training"]}
-//         />
-//       </div>
-//     </section>
-//   );
-// }
-
-
-
-
-
-
-
-
-
-
 import PricingCard from "./PricingCard";
 
 export default function Pricing() {
   return (
-    <section className="relative bg-[#030712] text-white py-24 px-6 overflow-hidden">
+    <section
+    id ="pricing"
+    className="relative bg-[#030712] text-white py-10 px-6 overflow-hidden">
       {/* Background Decorative Circles */}
       <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full pointer-events-none">
         <div className="absolute top-[-10%] left-[-20%] w-[600px] h-[600px] bg-indigo-500/5 blur-[120px] rounded-full" />
