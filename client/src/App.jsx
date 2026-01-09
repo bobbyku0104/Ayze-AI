38c491304e9f800fb9ce97477377164897db3926
bobbyku0104 <143bobby123@gmail.com>
2026-01-09 15:05:20 +0530

    remove useless routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,3 @@
-// import React from 'react'
-// import './App.css'
-
-
-
-// import Home from "./pages/Home";
-
-// export default function App() {
-//   return <Home />;
-// }
-
-
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/contact.jsx";
