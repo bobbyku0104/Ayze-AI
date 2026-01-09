99334bcc09afc928821ed9e6a6f4bf6fb102c10b
bobbyku0104 <143bobby123@gmail.com>
2026-01-09 12:39:46 +0530

    integarte trusted secton in home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Navbar from "../components/Navbar.jsx";
 import Hero from "../components/Hero.jsx";
+import TrustedSection from "../components/TrustedSection.jsx";
+
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <TrustedSection />
+      
     </>
   );
 }
